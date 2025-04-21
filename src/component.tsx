1e5bdaf50c611df0a56fb6f6f0a15a5b27a8384b
Jay <huny000315@gmail.com>
2025-04-21 13:16:08 +0900

    컴포넌트 생성 실습

---
+++ src/component.tsx
@@ -0,0 +1,18 @@
+const Component = () => {
+    return (
+        <div>
+            <h1>
+                hello react!
+            </h1>
+            <p>
+                리액트는 ui를 만드는 라이브러리 입니다.
+            </p>
+
+        </div>
+    );
+}
+
+export default Component;
+//const element = Component();
+
+//html 요소를 리턴으로 반환해야한다.
